82a115b720763773edb561ae60988b141bee529e
root <root@SURFACE.>
2024-03-17 13:34:43 +0300

    fx

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,54 +1,34 @@
 require ( './root.js' );
 
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
-  })
-})
+ function shout(string) {
+  return string.toUppercase();
+  }
 
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello');
-  })
-})
 
-describe('logShout(string)', function() {
-  it('takes a string argument and logs it in all caps using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
+function whisper(string) {
+  return string.toLowercase()
+  }
 
-    logShout('hello');
 
-    expect(spy).toHaveBeenCalledWith('HELLO');
+function logShout(string) {
+  console.log(string.toUppercase());
+  }
 
-    console.log.restore();
-  })
-})
+function logWhisper() {
+  
+    console.log(string.toLowercase());
+  }
 
-describe('logWhisper(string)', function() {
-  it('takes a string argument and logs it in all lowercase using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
 
-    logWhisper('HELLO');
 
-    expect(spy).toHaveBeenCalledWith('hello');
-
-    console.log.restore();
-  })
-})
-
-describe('sayHiToHeadphonedRoommate(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
-  })
-
-  it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
-    expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
-      "I would love to!"
-    );
-  });
-})
+ function sayHiToHeadphonedRoommate(string) {
+  if(string ===string.toUppercase()){
+  return "YES INDEED!"}
+  else if(string ===string.toLowercase()){
+    return "I cant hear you!"
+  }
+  else if(string ==="Lets have dinner together"){
+    return "I would love to!"
+  }
+}
